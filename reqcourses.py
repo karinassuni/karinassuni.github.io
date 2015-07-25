8fd73038f3ee756a9d146fe82f3053b26a4fdde3
karinassuni <karinassuni@users.noreply.github.com>
2015-07-25 00:08:48 -0700

    test domain.com for cgi-enabled

--- reqcourses.py
+++ reqcourses.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python
-# -*- coding: UTF-8 -*-
 
-# enable debugging
-import cgitb
-cgitb.enable()
+import sys
+import json
+import cgi
 
-print "Content-Type: text/plain;charset=utf-8"
-print
+fs = cgi.FieldStorage()
 
-print "Hello World!"
+sys.stdout.write("Content-Type: application/json")
+
+sys.stdout.write("\n")
+sys.stdout.write("\n")
+
+
+result = {}
+result['success'] = True
+result['message'] = "The command Completed Successfully"
+result['keys'] = ",".join(fs.keys())
+
+d = {}
+for k in fs.keys():
+    d[k] = fs.getvalue(k)
+
+result['data'] = d
+
+sys.stdout.write(json.dumps(result,indent=1))
+sys.stdout.write("\n")
+
+sys.stdout.close()
