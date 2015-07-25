78382ab33a1a3daed85b47a1c8a71422824f577d
karinassuni <karinassuni@users.noreply.github.com>
2015-07-25 00:03:39 -0700

    test domain.com for cgi-enabled

--- reqcourses.py
+++ reqcourses.py
@@ -1,23 +1,11 @@
-return "hi"
-import requests
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 
-URL = 'https://pbanssb.ucmerced.edu/pls/PROD/xhwschedule.P_ViewSchedule'
+# enable debugging
+import cgitb
+cgitb.enable()
 
-#Values for valid form submission to get to current schedule:
-values = {}
-values['validterm'] = 201530 #values[name] = value, e.g. <input name="validterm" value = "201530">
-values['subjcode'] = 'ALL'
-values['openclasses'] = 'N'
+print "Content-Type: text/plain;charset=utf-8"
+print
 
-print '\nRequesting access to live schedule...'
-r = requests.post(URL, data=values)
-
-print '\nCopying html...'
-html_f = open('C:/Users/Karlo Antonio/Documents/GitHub/karinassuni.github.io/__resources__/courses.html', 'w')
-rtx = str(r.text)
-html_f.write(rtx)
-html_f.close()
-
-import regexparser
-print '\nStarting parse...'
-regexparser.parse(html_f)
+print "Hello World!"
